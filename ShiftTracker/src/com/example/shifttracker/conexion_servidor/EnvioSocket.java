651cae9b47b5d2eea2adf2ba9918fc8feaae6ee3
MonicaVDS <vdsbulldog@hotmail.com>
2024-03-23 20:51:50 +0100

    Commit initial

---
+++ ShiftTracker/src/com/example/shifttracker/conexion_servidor/EnvioSocket.java
@@ -0,0 +1,25 @@
+package com.example.shifttracker.conexion_servidor;
+
+import java.io.Serializable;
+import java.util.Map;
+
+public class EnvioSocket implements Serializable {
+	
+	private String tipo;
+	Map <String, Object> objetosAdjuntos;
+	
+	public EnvioSocket(String tipo, Map<String, Object> objetosAdjuntos) {
+		this.tipo = tipo;
+		this.objetosAdjuntos = objetosAdjuntos;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public Map<String, Object> getObjetosAdjuntos() {
+		return objetosAdjuntos;
+	}
+	
+}
